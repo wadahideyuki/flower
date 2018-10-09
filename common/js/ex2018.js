bb80a62fcb30d164b2d448c9bf6e697beb5c4bed
smasuzawa <koubousei@gmail.com>
2018-10-09 18:29:41 +0900

    残りは画像入れ

--- common/js/ex2018.js
+++ common/js/ex2018.js
@@ -1,11 +1,37 @@
 $(function(){
+  $(".modal").click(function(){
+    $(this).fadeOut();
+  });
   $(".modal-close").click(function(){
     $(".modal").fadeOut();
   });
-  $("a.greet").click(function(){
+  $(".col2-btn span").click(function(){
+    var thisCont = $(this).attr("class");
     $(".modal").css({display:"flex"});
+
+    $(".mdlCont").hide();
+    $(".mdlCont." + thisCont).show();
+    return false;
   });
-  $("a.about").click(function(){
-    $(".modal").css({display:"flex"});
+
+  $(".modal-inner").click(function(e){
+    e.stopPropagation();
   });
-})
+
+
+  $(".exhibitor-box .tabs h3").click(function(){
+    var idx = $(".exhibitor-box .tabs h3").index(this);
+
+    $(".exhibitor-box .tabs h3").removeClass("show");
+    $(this).addClass("show");
+
+    $(".exhibitor-box .box").removeClass("show");
+    $(".exhibitor-box .box").eq(idx).addClass("show");
+    return false;
+  });
+
+
+
+
+
+});
